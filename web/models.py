9938783e4b5381d7185789df83fbb1c2ea226df4
Omar Estrella <oestrella@rallydev.com>
2013-05-12 18:45:25 -0400

    Company model and requirements

--- web/models.py
+++ web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Company(models.Model):
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=255)
+    url = models.URLField(max_length=255)
+    description = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
